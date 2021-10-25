ea53d9c82c33bca3ff1246b6c01f80b28157a257
aniket32 <aniketbasu32@gmail.com>
2021-10-25 20:36:09 +0100

    added randint function

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ def b(x):
         fitness += x[i] ** 2 - (10 * math.cos(2*math.pi*x[i]))
     return fitness
 
+# First try to create 30 constraints for 30 dimensionality
+# min = [random.randint(-10, 10) for i in range(0, 30)]
+# max = [random.randint(1, 10) for j in range(0, 30)]
 
 min = [-5.12] * 30
 max = [5.12] * 30
